aaad63566f45f93ee8919c9c77a9ef11f8efbf3a
Roukaya Chelly <roukayachelly7@gmail.com>
2024-08-13 22:12:20 +0100

    upsate

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return(
-    <Button>
-      click me
-    </Button>
+    <p>
+     this is an authenticated route
+    </p>
   )
 }
