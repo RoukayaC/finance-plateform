ab5f9ca2f4b5c428dc79428a4edcb6b8cd36f8e1
Roukaya Chelly <roukayachelly7@gmail.com>
2024-08-16 22:50:02 +0100

    update

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,23 +1,18 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import {z} from 'zod'
-import { zValidator} from '@hono/zod-validator'
-export const runtime = 'edge'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+export const runtime = "edge";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { error } from "console";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/hello',
-    clerkMiddleware(),
-    (c) => {
-        const auth = getAuth(c)
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+app.get(
+  "/hello",
+   clerkMiddleware(),
+  helloController(),
+  );
 
-
-  
-
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
