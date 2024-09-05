44accc4cc7a2661452e96bd26526d9173ab1169e
Roukaya Chelly <roukayachelly7@gmail.com>
2024-09-05 10:34:38 +0100

    config drizzle orm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./db/schema.ts",
-  driver: "pg",
+  driver: "pg", 
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
