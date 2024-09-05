70c593de9ff9ba93b27e579806b04bf5ddbfc5a4
Roukaya Chelly <roukayachelly7@gmail.com>
2024-09-05 23:23:38 +0100

    updates

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,16 @@
 import { Header } from "@/components/header";
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
-
-const DashboardLayout = ({children}: Props) => {
+const DashboardLayout = ({ children }: Props) => {
   return (
     <>
-    <Header/>
-    <main className="px-3 lg:px-14">
-        {children}
-    </main>
+      <Header />
+      <main className="px-3 lg:px-14">{children}</main>
     </>
-    );
+  );
 };
 
 export default DashboardLayout;
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -18,22 +18,6 @@ type FormValues = z.input<typeof formSchema>;
 export const NewAccountSheet = () => {
   const onSubmit = (values: FormValues) => {};
   return (
-    <Sheet open={} onOpenChange={}>
-      <SheetContent className="space-y-4">
-        <SheetHeader>
-          <SheetTitle>New Account</SheetTitle>
-          <SheetDescription>
-            Create a new account to track your transactions.
-          </SheetDescription>
-        </SheetHeader>
-        {/* <AccountForm
-          onSubmit={onSubmit}
-          disabled={mutation.isPending}
-          defaultValues={{
-            name: "",
-          }}
-        /> */}
-      </SheetContent>
-    </Sheet>
+    <></>
   );
 };
