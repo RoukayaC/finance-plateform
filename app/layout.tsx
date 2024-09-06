34e09ed8a3d7777fbc57542611f38b61d44f8622
Roukaya Chelly <roukayachelly7@gmail.com>
2024-09-06 14:58:30 +0100

    post api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { clerkMiddleware } from "@clerk/nextjs/server";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
-import { Toaster } from "sonner";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { useCreateAccount } from "../api/use-create-account";
+
 import { insertAccountSchema } from "@/db/schema";
 import {
   Sheet,
@@ -16,8 +20,34 @@ const formSchema = insertAccountSchema.pick({
 type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
-  const onSubmit = (values: FormValues) => {};
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
   return (
-    <></>
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
+      </SheetContent>
+    </Sheet>
   );
 };
