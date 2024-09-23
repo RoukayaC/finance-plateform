8e704f2c3eb71b975479223cbd989a48ede59162
Roukaya Chelly <roukayachelly7@gmail.com>
2024-09-23 09:23:38 +0100

    Transactions Hooks

--- features/transactions/api/use-bulk-create-transactions.ts
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -5,28 +5,28 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
 
 type ResponseType = InferResponseType<
-  (typeof client.api.accounts)["bulk-delete"]["$post"]
+  (typeof client.api.transactions)["bulk-create"]["$post"]
 >;
 type RequestType = InferRequestType<
-  (typeof client.api.accounts)["bulk-delete"]["$post"]
+  (typeof client.api.transactions)["bulk-create"]["$post"]
 >["json"];
 
-export const useBulkDeleteAccounts = () => {
+export const useBulkCreateTransactions = () => {
   const queryClient = useQueryClient();
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async (json) => {
-      const response = await client.api.accounts["bulk-delete"]["$post"]({
+      const response = await client.api.transactions["bulk-create"]["$post"]({
         json,
       });
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Accounts deleted");
-      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      toast.success("Transactions created");
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
       queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
     onError: () => {
-      toast.error("Failed to delete accounts");
+      toast.error("Failed to create transactions");
     },
   });
   return mutation;
