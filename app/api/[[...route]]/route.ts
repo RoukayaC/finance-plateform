39f57dce65c387b70b287821775da115d6fcf434
Roukaya Chelly <roukayachelly7@gmail.com>
2024-09-23 09:01:32 +0100

    Transactions

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,20 +1,20 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import categories from "./categories";
+import transactions from "./transactions";
+import accounts from "./accounts";
 
 // import summary from "./summary";
-// import transactions from "./transactions";
-import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
-   .route("/categories", categories)
-   .route("/accounts", accounts);
-  // .route("/summary", summary)
-  // .route("/transactions", transactions)
+  .route("/categories", categories)
+  .route("/accounts", accounts)
+  .route("/transactions", transactions);
+// .route("/summary", summary)
 
 export const GET = handle(app);
 export const POST = handle(app);
